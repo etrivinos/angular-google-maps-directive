42f8bcd7cbb0db97847d7e23e897b28e32737f8a
Edwin A. Trivinos R <edwin.trivinos@hotmail.com>
2016-03-04 09:02:00 -0500

    Set lat & lon attributes for the directive and define the link function

--- libs/md-google-maps.js
+++ libs/md-google-maps.js
@@ -10,10 +10,26 @@
     return {
       restrict: 'E',
       replace: true,
-      scope: {},
+      scope: {
+        lat: '=',
+        lng: '=',
+        zoom: '='
+      },
       template: '<div id="md-google-maps"></div>',
       link: function(scope, element, attrs) {
-         
+        var center = new google.maps.LatLng(scope.lat, scope.lng);
+        var zoom = scope.zoom || 12;
+        var mapSettings = {
+          center: center,
+          zoom: zoom
+        };
+        var map = {};
+             
+        initialize();
+       
+        function initialize() {
+          map = new google.maps.Map(element[0], mapSettings);
+        }
       }
     }
   }
